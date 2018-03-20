7cd99b41dfe069d92e41b5dd1dea9348e8cdd084
Arslan Ameer <arslan.ameer@hotmail.com>
2018-03-20 21:03:22 +0500

    Update: Stars Img with Rating Feature
    Added star images with hover and select  rating, and get number of selected stars.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,47 +1,48 @@
 
-document.addEventListener("DOMContentLoaded",() => {
+document.addEventListener("DOMContentLoaded", () => {
 
     var star = document.getElementsByClassName("star-rating");
-
+    var starCount;
+    
     for (let i = 0; i < star.length; i++) {
         
         const el = star[i];
-
+        
         el.addEventListener("mouseover", () => {
             const attrVal = el.getAttributeNode("value").value;
             for (let j = 0; j < attrVal; j++) {
                 const el2 = star[j];
-                if (el2.style.backgroundColor !== "red") {
-                    el2.style.backgroundColor = "yellow";                    
-                }
+                el2.classList.add("hoverimg");
             }
         });
-
+        
         el.addEventListener("mouseout", () => {
             const attrVal = el.getAttributeNode("value").value;
             for (let j = 0; j < attrVal; j++) {
                 const el2 = star[j];
-                if (el2.style.backgroundColor !== "red") {
-                    el2.style.backgroundColor = "white";                    
-                }
+                el2.classList.remove("hoverimg");
             }
         });
-
-        el.addEventListener("click",() => {
+        
+        el.addEventListener("click", () => {
             const attrVal = el.getAttributeNode("value").value;
+            starCount = 0;
             for (let j = 0; j < attrVal; j++) {
                 const el2 = star[j];
-                if (el2.style.backgroundColor === "yellow") {
-                    el2.style.backgroundColor = "red"; 
-                }else{
-                    for (let k = attrVal; k < star.length; k++) {
-                        star[k].style.backgroundColor = "white";                 
-                    }
+                if (el2.classList.contains("hoverimg")) {
+                    el2.classList.add("starON");
+                    starCount++;
                 }
-                
+                for (let k = attrVal; k < star.length; k++) {
+                    star[k].classList.remove("starON");
+                }
             }
         });
     }
-    
+
+    document.getElementById("starCount").addEventListener('click', () => {
+        alert(starCount);
+    });
+
 });
 
